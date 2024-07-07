8caf6da413aca74e9974922b6c7e7397f700bbfd
LeeDongHoon <likelion.leedh@gmail.com>
2024-07-07 21:44:40 +0900

    주문, 주문 상품 엔티티 추가

---
+++ server/src/main/java/com/onetool/server/order/Order.java
@@ -0,0 +1,31 @@
+package com.onetool.server.order;
+
+import com.onetool.server.global.entity.BaseEntity;
+import com.onetool.server.payments.Payments;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long totalPrice;
+
+    private int totalCount;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    private Payments payments;
+
+}
