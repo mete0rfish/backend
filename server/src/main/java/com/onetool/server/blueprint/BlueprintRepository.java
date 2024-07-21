ec06229f418be0091ebfe9e5967f70c30c80d959
(sungwon) <(sungwon326@naver.com)>
2024-07-21 21:56:45 +0900

    fix: BlueprintRepository @Query의 @Param 추가

--- server/src/main/java/com/onetool/server/blueprint/BlueprintRepository.java
+++ server/src/main/java/com/onetool/server/blueprint/BlueprintRepository.java
@@ -11,11 +11,11 @@ import org.springframework.stereotype.Repository;
 public interface BlueprintRepository extends JpaRepository<Blueprint, Long> {
 
     @Query(value = "SELECT b FROM Blueprint b WHERE b.blueprintName LIKE %:keyword% OR b.creatorName LIKE %:keyword%")
-    Page<Blueprint> findAllNameAndCreatorContaining(String keyword, Pageable pageable);
+    Page<Blueprint> findAllNameAndCreatorContaining(@Param("keyword") String keyword, Pageable pageable);
 
     @Query(value = "SELECT b FROM Blueprint b WHERE b.categoryId = " +
-            "(SELECT f.id FROM FirstCategory f WHERE f.name = :category)")
-    Page<Blueprint> findAllByFirstCategory(String category, Pageable pageable);
+            "(SELECT f.id FROM FirstCategory f WHERE f.name = :first)")
+    Page<Blueprint> findAllByFirstCategory(@Param("first") String category, Pageable pageable);
 
     @Query(value = "SELECT b FROM Blueprint b WHERE b.secondCategory = :second " +
             "AND b.categoryId = (SELECT f.id FROM FirstCategory f WHERE f.name = :first)")
