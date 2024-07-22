c34b48694455441966678932eced4e9ce908a4f1
LeeDongHoon <likelion.leedh@gmail.com>
2024-07-22 17:50:38 +0900

    [chore] QnaBoard, QnaReply 엔티티 수정 및 매핑 메서드, 정적 팩토리 메서드 추가

--- server/src/main/java/com/onetool/server/qna/QnaBoard.java
+++ server/src/main/java/com/onetool/server/qna/QnaBoard.java
@@ -2,21 +2,24 @@ package com.onetool.server.qna;
 
 import com.onetool.server.global.entity.BaseEntity;
 import com.onetool.server.member.Member;
+import com.onetool.server.qna.dto.request.QnaRequest;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
-@Table(name = "qna_board")
+@Getter
+@Table(name = "qna_board", indexes = @Index(name = "idx_qna", columnList = "qna_board_title, member_id, created_at"))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class QnaBoard extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "qna_board_title") @NotNull
     @Size(min = 2, max = 30, message = "제목은 2 ~ 30자 이여야 합니다.")
@@ -27,16 +30,25 @@ public class QnaBoard extends BaseEntity {
     @OneToMany(mappedBy = "qnaBoard")
     private List<QnaReply> qnaReplies;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
     @Builder
-    private QnaBoard(String title, String content, List<QnaReply> qnaReplies, Member member) {
+    private QnaBoard(String title, String content) {
         this.title = title;
         this.content = content;
-        this.qnaReplies = qnaReplies;
+    }
+
+    public static QnaBoard createQnaBoard(QnaRequest.PostQnaBoard request){
+        return new QnaBoard(request.title(),
+                request.content());
+
+    }
+
+    public void post(Member member){
         this.member = member;
+        member.getQnaBoards().add(this);
     }
     // TODO builder패턴 완성하기
 }
