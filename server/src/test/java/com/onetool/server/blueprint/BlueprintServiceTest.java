16d3653e40e031be9b277132620afec6959d7851
DaYeong <day0_ng@naver.com>
2024-08-24 20:42:43 +0900

    [TIC-83]refactor: BlueprintController응답 ApiResponse로 통일

--- server/src/test/java/com/onetool/server/blueprint/BlueprintServiceTest.java
+++ server/src/test/java/com/onetool/server/blueprint/BlueprintServiceTest.java
@@ -5,6 +5,7 @@ import com.onetool.server.blueprint.service.BlueprintService;
 import com.onetool.server.blueprint.dto.BlueprintRequest;
 import com.onetool.server.blueprint.dto.BlueprintResponse;
 import com.onetool.server.blueprint.dto.SearchResponse;
+import com.onetool.server.global.exception.ApiResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigInteger;
@@ -57,40 +56,37 @@ public class BlueprintServiceTest {
                 "https://onetool.com/download"
         );
 
-        ResponseEntity<String> response = blueprintController.createBlueprint(blueprintRequest);
+        ApiResponse<String> response = blueprintController.createBlueprint(blueprintRequest);
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isEqualTo("상품이 정상적으로 등록되었습니다.");   }
+        assertThat(response.getResult()).isEqualTo("상품이 정상적으로 등록되었습니다.");
+    }
 
     @DisplayName("blueprint id를 통해 상세 정보 조회가 되는지 확인")
     @Test
     public void testGetBlueprintDetails() {
         Long blueprintId = 1L;
 
-        ResponseEntity<BlueprintResponse> response = blueprintController.getBlueprintDetails(blueprintId);
+        ApiResponse<BlueprintResponse> response = blueprintController.getBlueprintDetails(blueprintId);
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getMessage()).isEqualTo("요청에 성공하였습니다.");
+    }
 
-        BlueprintResponse responseBody = response.getBody();
-        assertThat(responseBody.id()).isEqualTo(blueprintId);
-        assertThat(responseBody.blueprintName()).isEqualTo("대한민국 마을");
-        assertThat(responseBody.blueprintDetails()).isEqualTo("대한민국의 어느 마을의 청사진입니다.");
-        assertThat(responseBody.creatorName()).isEqualTo("윤성원 작가");
-        assertThat(responseBody.standardPrice()).isEqualTo(50000L);
-        assertThat(responseBody.salePrice()).isEqualTo(40000L);
-        assertThat(responseBody.program()).isEqualTo("CAD");
-        assertThat(responseBody.downloadLink()).isEqualTo("https://onetool.com/download");
-        assertThat(responseBody.extension()).isEqualTo(".exe");
-        assertThat(responseBody.blueprintImg()).isEqualTo("https://s3.bucket.image.com/");
-        assertThat(responseBody.categoryId()).isEqualTo(1L);
+    @DisplayName("blueprint가 정상적으로 삭제되는지 확인")
+    @Test
+    public void testDeleteBlueprint() {
+        Long blueprintId = 1L;
+
+        ApiResponse<?> response = blueprintController.deleteBlueprint(blueprintId);
+
+        assertThat(response.getMessage()).isEqualTo("요청에 성공하였습니다.");
+        assertThat(response.getResult()).isEqualTo("상품이 정상적으로 삭제 되었습니다.");
     }
 
     @DisplayName("blueprint가 정상적으로 업데이트되는지 확인")
     @Test
     public void testUpdateBlueprint() {
         BlueprintResponse blueprintResponse = new BlueprintResponse(
-                1L,
+                653L,
                 "대한민국 마을",
                 1L,
                 50000L,
@@ -105,20 +101,9 @@ public class BlueprintServiceTest {
                 "https://onetool.com/download"
         );
 
-        ResponseEntity<Boolean> response = blueprintController.updateBlueprint(blueprintResponse);
+        ApiResponse<?> response = blueprintController.updateBlueprint(blueprintResponse);
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isTrue();
-    }
-
-    @DisplayName("blueprint가 정상적으로 삭제되는지 확인")
-    @Test
-    public void testDeleteBlueprint() {
-        Long blueprintId = 1L;
-
-        ResponseEntity<Boolean> response = blueprintController.deleteBlueprint(blueprintId);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isTrue();
+        assertThat(response.getMessage()).isEqualTo("요청에 성공하였습니다.");
+        assertThat(response.getResult()).isEqualTo("상품이 정상적으로 수정 되었습니다.");
     }
 }
