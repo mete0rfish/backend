a3e5e8c9a73dfcf50d9dbf7e318c7cda68f16e4f
mete0rfish <sungwon326@naver.com>
2024-09-06 20:45:43 +0900

    [TIC-93] feat: Refresh Token 생성
    - Jwts를 이용한 Refresh Token 생성 메서드
    - Claims로 User의 Id를 사용

--- server/src/main/java/com/onetool/server/global/auth/jwt/JwtUtil.java
+++ server/src/main/java/com/onetool/server/global/auth/jwt/JwtUtil.java
@@ -2,6 +2,8 @@ package com.onetool.server.global.auth.jwt;
 
 import com.onetool.server.global.auth.AuthorizationProvider;
 import com.onetool.server.global.auth.MemberAuthContext;
+import com.onetool.server.global.redis.domain.Token;
+import com.onetool.server.global.redis.repository.TokenRepository;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -13,6 +15,7 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.Base64;
 import java.util.Date;
@@ -28,15 +31,19 @@ public class JwtUtil implements AuthorizationProvider {
 
     private final SecretKey key;
     private final Long expirationMilliSec;
+    // TODO application으로 지정하기
+    private static final int refreshTokenExpirationMillis = 864_000_000;
+    private final TokenRepository tokenRepository;
 
     public JwtUtil(
             @Value("${onetool.jwt.secrekey}") String secretKey,
-            @Value("${onetool.jwt.expiration_time}") Long expirationMilliSec
-    ) {
+            @Value("${onetool.jwt.expiration_time}") Long expirationMilliSec,
+            TokenRepository tokenRepository) {
         byte[] keyBytes = Base64.getDecoder()
                 .decode(secretKey.getBytes(StandardCharsets.UTF_8));
         this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
         this.expirationMilliSec = expirationMilliSec;
+        this.tokenRepository = tokenRepository;
     }
 
     @Override
@@ -48,7 +55,6 @@ public class JwtUtil implements AuthorizationProvider {
 
         ZonedDateTime now = ZonedDateTime.now();
         ZonedDateTime tokenValidity = now.plusSeconds(expirationMilliSec);
-
 
         return Jwts.builder()
                 .setClaims(claims)
@@ -88,5 +94,25 @@ public class JwtUtil implements AuthorizationProvider {
     public Long getUserId(String token) {
         Double db = parseClaims(token).get("memberId", Double.class);
         return db.longValue();
+    }
+
+    @Override
+    public String createRefreshToken(Long id, Instant issuedAt) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", id);
+
+        String refreshToken = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(Date.from(issuedAt))
+                .setExpiration(Date.from(issuedAt.plusMillis(refreshTokenExpirationMillis)))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        Token token = Token.builder()
+                .refreshToken(refreshToken)
+                .build();
+
+        tokenRepository.save(token);
+        return refreshToken;
     }
 }
