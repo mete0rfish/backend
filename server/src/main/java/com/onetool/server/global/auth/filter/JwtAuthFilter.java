0174080159e25b73367f08302822bd48d69f3daf
mete0rfish <sungwon326@naver.com>
2024-09-09 16:07:28 +0900

    [TIC-93] fix: JwtAuthFilter의 주석 삭제

--- server/src/main/java/com/onetool/server/global/auth/filter/JwtAuthFilter.java
+++ server/src/main/java/com/onetool/server/global/auth/filter/JwtAuthFilter.java
@@ -34,16 +34,14 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authorizationHeader = request.getHeader("Authorization");
 
-        //JWT가 헤더에 있는 경우
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             String token = authorizationHeader.substring(7);
             log.info("토큰: " + token);
-            //JWT 유효성 검증
+
             if (checkAccessTokenValid(token)) {
                 log.info("validation 통과");
                 Long userId = jwtUtil.getUserId(token);
 
-                //유저와 토큰 일치 시 principalDetails 생성
                 PrincipalDetails principalDetails = customUserDetailsService.loadUserByUsername(userId.toString());
 
                 if (principalDetails != null) {
