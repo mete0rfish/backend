7ed2970e0c512933477702d0e53a9866042afcd4
mete0rfish <sungwon326@naver.com>
2024-09-25 16:32:16 +0900

    feat: 도면 삭제 기능 구현

--- server/src/main/java/com/onetool/server/blueprint/controller/BlueprintInspectionController.java
+++ server/src/main/java/com/onetool/server/blueprint/controller/BlueprintInspectionController.java
@@ -3,7 +3,12 @@ package com.onetool.server.blueprint.controller;
 import com.onetool.server.blueprint.dto.BlueprintResponse;
 import com.onetool.server.blueprint.service.BlueprintInspectionService;
 import com.onetool.server.global.exception.ApiResponse;
+import com.onetool.server.global.exception.codes.ErrorCode;
+import com.onetool.server.member.domain.Member;
+import com.onetool.server.member.enums.UserRole;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,13 +27,14 @@ public class BlueprintInspectionController {
     }
 
     @PostMapping("/inspection")
-    public ApiResponse<?> approveBluprint(@RequestBody Long id) {
+    public ApiResponse<?> approveBlueprint(@RequestBody Long id) {
         blueprintInspectionService.approveBlueprint(id);
         return ApiResponse.onSuccess("승인이 완료되었습니다");
     }
 
-   /* @DeleteMapping("/inspection")
-    public ApiResponse<?> rejectBluprint(@RequestBody Long id) {
-
-    }*/
+    @DeleteMapping("/inspection")
+    public ApiResponse<?> rejectBlueprint(@RequestBody Long id) {
+        blueprintInspectionService.rejectBlueprint(id);
+        return ApiResponse.onSuccess("반려(삭제)가 완료되었습니다.");
+    }
 }
