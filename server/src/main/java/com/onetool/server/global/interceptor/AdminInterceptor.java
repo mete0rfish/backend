7a99562176d6ed0dc28819ccef328fef435eeaf5
mete0rfish <sungwon326@naver.com>
2024-09-27 19:31:42 +0900

    feat: /admin/* 경로는 관리자 계정만 접근가능하도록 Interceptor 설정

---
+++ server/src/main/java/com/onetool/server/global/interceptor/AdminInterceptor.java
@@ -0,0 +1,30 @@
+package com.onetool.server.global.interceptor;
+
+import com.onetool.server.global.auth.jwt.JwtUtil;
+import com.onetool.server.member.enums.UserRole;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class AdminInterceptor implements HandlerInterceptor {
+
+    private final JwtUtil jwtUtil;
+
+    public AdminInterceptor(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String tokenWithBearer = request.getHeader("Authorization");
+        String token = tokenWithBearer.substring(7);
+        String role = jwtUtil.parseClaims(token).get("role", String.class);
+        if(role.equals(UserRole.ROLE_ADMIN.name())) {
+            return true;
+        }
+        return false;
+    }
+}
