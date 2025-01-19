8d5a698c169b1586f6ad8eeed71f9c56a7200e3a
mete0rfish <sungwon326@naver.com>
2025-01-19 15:47:56 +0900

    fix: SearchResponse 수정

--- server/src/main/java/com/onetool/server/api/blueprint/dto/SearchResponse.java
+++ server/src/main/java/com/onetool/server/api/blueprint/dto/SearchResponse.java
@@ -1,20 +1,46 @@
 package com.onetool.server.api.blueprint.dto;
 
 import com.onetool.server.api.blueprint.Blueprint;
+import jakarta.persistence.Column;
 import lombok.Builder;
 
-public record SearchResponse(
-        Blueprint blueprint
-) {
+import java.math.BigInteger;
+import java.time.LocalDateTime;
 
-    @Builder
-    public SearchResponse(Blueprint blueprint) {
-        this.blueprint = blueprint;
-    }
+@Builder
+public record SearchResponse(
+        Long id,
+        String blueprintName,
+        Long categoryId,
+        Long standardPrice,
+        String blueprintImg,
+        String blueprintDetails,
+        String extension,
+        String program,
+        BigInteger hits,
+        Long salePrice,
+        LocalDateTime saleExpiredDate,
+        String creatorName,
+        String downloadLink,
+        String secondCategory
+) {
 
     public static SearchResponse from(Blueprint blueprint) {
         return SearchResponse.builder()
-                .blueprint(blueprint)
+                .id(blueprint.getId())
+                .blueprintName(blueprint.getBlueprintName())
+                .categoryId(blueprint.getCategoryId())
+                .standardPrice(blueprint.getStandardPrice())
+                .blueprintImg(blueprint.getBlueprintImg())
+                .blueprintDetails(blueprint.getBlueprintDetails())
+                .extension(blueprint.getExtension())
+                .program(blueprint.getProgram())
+                .hits(blueprint.getHits())
+                .salePrice(blueprint.getSalePrice())
+                .saleExpiredDate(blueprint.getSaleExpiredDate())
+                .creatorName(blueprint.getCreatorName())
+                .downloadLink(blueprint.getDownloadLink())
+                .secondCategory(blueprint.getSecondCategory())
                 .build();
     }
 }
