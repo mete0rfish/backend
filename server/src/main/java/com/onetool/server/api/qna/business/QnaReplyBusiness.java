2890b967f76f0765cbe528b81233dd0da7392548
PlusUltraCode <kkd06144@naver.com>
2025-02-24 14:53:23 +0900

    feat(qnaReply): qnaReply (Business 및 Converter) 계층 생성

---
+++ server/src/main/java/com/onetool/server/api/qna/business/QnaReplyBusiness.java
@@ -0,0 +1,9 @@
+package com.onetool.server.api.qna.business;
+
+import com.onetool.server.global.annotation.Business;
+import lombok.RequiredArgsConstructor;
+
+@Business
+@RequiredArgsConstructor
+public class QnaReplyBusiness {
+}
---
+++ server/src/main/java/com/onetool/server/api/qna/converter/QnaReplyConverter.java
@@ -0,0 +1,10 @@
+package com.onetool.server.api.qna.converter;
+
+import com.onetool.server.global.annotation.Converter;
+import lombok.RequiredArgsConstructor;
+
+@Converter
+@RequiredArgsConstructor
+public class QnaReplyConverter {
+
+}
