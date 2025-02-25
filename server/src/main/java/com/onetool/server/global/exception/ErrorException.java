0d75785ed4ca04ef778ac1333ab89f5bf2a578f5
PlusUltraCode <kkd06144@naver.com>
2025-02-25 10:31:14 +0900

    refactor(qnaBoard): 기존 ErrorExecption을 현제 템플릿에 맞게 이름과 구조 변경

--- server/src/main/java/com/onetool/server/global/exception/ErrorException.java
+++
@@ -1,7 +0,0 @@
-package com.onetool.server.global.exception;
-
-public class ErrorException extends RuntimeException{
-    public ErrorException(String errorMessage) {
-        super(errorMessage);
-    }
-}
