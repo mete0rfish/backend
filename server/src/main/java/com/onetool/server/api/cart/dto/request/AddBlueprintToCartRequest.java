8031af94dabc66e357f1b67ec601176cb0322da1
PlusUltraCode <kkd06144@naver.com>
2025-02-27 17:03:24 +0900

    refactor(Cart): 기존 DTO 분리

---
+++ server/src/main/java/com/onetool/server/api/cart/dto/request/AddBlueprintToCartRequest.java
@@ -0,0 +1,7 @@
+package com.onetool.server.api.cart.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record AddBlueprintToCartRequest(
+        @NotNull Long blueprintId
+){}
---
+++ server/src/main/java/com/onetool/server/api/cart/dto/response/CartItemsResponse.java
@@ -0,0 +1,23 @@
+package com.onetool.server.api.cart.dto.response;
+
+import com.onetool.server.api.blueprint.dto.response.BlueprintResponse;
+import com.onetool.server.api.cart.CartBlueprint;
+import com.onetool.server.api.cart.dto.CartResponse;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record CartItemsResponse(
+        Long totalPrice,
+        List<BlueprintResponse> blueprintsInCart
+){
+    public static CartItemsResponse cartItems(Long totalPrice, List<CartBlueprint> cartBlueprints){
+        return CartItemsResponse.builder()
+                .totalPrice(totalPrice)
+                .blueprintsInCart(cartBlueprints.stream()
+                        .map(cartItem -> BlueprintResponse.items(cartItem.getBlueprint()))
+                        .toList())
+                .build();
+    }
+}
