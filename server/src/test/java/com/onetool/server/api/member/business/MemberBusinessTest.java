7ae9fd022ca1029a0961ea02762c11dd87c99df6
mete0rfish <sungwon326@naver.com>
2025-03-10 10:03:47 +0900

    test(member): Mockito를 이용한 Business 테스트 코드 추가

--- server/src/test/java/com/onetool/server/api/member/business/MemberBusinessTest.java
+++ server/src/test/java/com/onetool/server/api/member/business/MemberBusinessTest.java
@@ -1,29 +1,32 @@
 package com.onetool.server.api.member.business;
 
+import com.onetool.server.api.member.fixture.MemberFixture;
 import com.onetool.server.api.member.domain.Member;
 import com.onetool.server.api.member.dto.*;
-import com.onetool.server.api.member.enums.UserRole;
-import com.onetool.server.api.member.fake.FakeMemberRepository;
 import com.onetool.server.api.member.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class MemberBusinessTest {
 
+    @InjectMocks
     private MemberBusiness memberBusiness;
+
+    @Mock
     private MemberService memberService;
-    private FakeMemberRepository memberRepository;
+
+    @Mock
     private PasswordEncoder encoder;
 
     private Member member;
@@ -31,131 +34,80 @@ class MemberBusinessTest {
 
     @BeforeEach
     void setup() {
-        memberRepository = new FakeMemberRepository();
-        memberService = new MemberService(memberRepository);
-        encoder = new BCryptPasswordEncoder();
-        memberBusiness = new MemberBusiness(encoder, memberService);
+        MockitoAnnotations.openMocks(this);
 
-        admin = memberRepository.save(
-                Member.builder()
-                        .name("관리자 윤성원")
-                        .password(encoder.encode("1234"))
-                        .email("admin@example.com")
-                        .role(UserRole.ROLE_ADMIN)
-                        .phoneNum("01012345678")
-                        .field("백엔드")
-                        .isNative(true)
-                        .build()
-        );
+        member = MemberFixture.createMember();
+        member.setPassword(encoder.encode("1234"));
 
-        member = memberRepository.save(
-                Member.builder()
-                        .name("홍길동")
-                        .password(encoder.encode("1234"))
-                        .email("user@example.com")
-                        .role(UserRole.ROLE_ADMIN)
-                        .phoneNum("01000000000")
-                        .field("백엔드")
-                        .isNative(true)
-                        .build()
-        );
+        admin = MemberFixture.createAdmin();
+        admin.setPassword(encoder.encode("1234"));
     }
 
     @Test
-    void 이메일로_회원을_조회한다() {
+    void 회원의_이메일을_조회한다() {
         // given
         MemberFindEmailRequest request = new MemberFindEmailRequest("홍길동", "01000000000");
+        when(memberService.findOne(request.name(), request.phone_num())).thenReturn(member);
 
         // when
         String email = memberBusiness.findEmail(request);
 
         // then
         assertThat(email).isEqualTo(member.getEmail());
+        verify(memberService, times(1)).findOne(request.name(), request.phone_num());
     }
 
     @Test
     void 회원의_정보를_조회한다() {
         // given
-        final Long userId = member.getId();
+        when(memberService.findOne(member.getId())).thenReturn(member);
 
         // when
-        MemberInfoResponse response = memberBusiness.getMemberInfo(userId);
+        MemberInfoResponse response = memberBusiness.getMemberInfo(member.getId());
 
         // then
         assertThat(response.email()).isEqualTo(member.getEmail());
-    }
-
-    /*
-    구매한 도면을 조회하기 위해선 주문 데이터를 생성해야 한다.
-    MemberTest에서 주문 데이터를 생성하는 과정이 포함되어도 되는 것일까?
-     */
-    @Test
-    void 회원의_구매한_도면목록을_조회한다() {
-        // given
-        final Long userId = member.getId();
-
-        // when
-        List<BlueprintDownloadResponse> response = memberBusiness.getPurchasedBlueprints(userId);
-
-        // then
-
+        verify(memberService, times(1)).findOne(member.getId());
     }
 
     @Test
     void 회원을_생성한다() {
         // given
-        final String email = "iamuser12@gmail.com";
-        final String name = "임홍빈";
-
-        MemberCreateRequest request = new MemberCreateRequest(
-                email,
-                "iamuser123",
-                name,
-                "2024-01-01",
-                "BACKEND",
-                "01034235512",
-                true
-        );
+        MemberCreateRequest request = MemberFixture.createMemberCreateRequest();
+        when(memberService.save(any(Member.class))).thenReturn(member);
 
         // when
         MemberCreateResponse response = memberBusiness.createMember(request);
 
         // then
-        assertThat(response.email()).isEqualTo(email);
-        assertThat(response.name()).isEqualTo(name);
+        assertThat(response.email()).isEqualTo(member.getEmail());
+        assertThat(response.name()).isEqualTo(member.getName());
+        verify(memberService, times(1)).save(any(Member.class));
     }
 
     @Test
     void 회원의_이름을_업데이트한다() {
         // given
-        final Long userId = member.getId();
-        final String name = "동길홍";
-        MemberUpdateRequest request = MemberUpdateRequest.builder()
-                .name(name)
-                .build();
+        MemberUpdateRequest request = MemberFixture.createMemberUpdateRequest();
+        when(memberService.findOne(member.getId())).thenReturn(member);
 
         // when
-        memberBusiness.updateMember(userId, request);
+        memberBusiness.updateMember(member.getId(), request);
 
         // then
-        String updatedName = memberRepository.findById(userId)
-                .orElseThrow()
-                .getName();
-        assertThat(updatedName).isEqualTo(name);
+        verify(memberService).save(member);
     }
 
     @Test
     void 회원을_삭제한다() {
         // given
-        final Long userId = member.getId();
+        when(memberService.findOne(member.getId())).thenReturn(member);
+        doNothing().when(memberService).delete(member);
 
         // then
-        memberBusiness.deleteMember(userId);
+        memberBusiness.deleteMember(member.getId());
 
         // when
-        assertThatThrownBy(() -> memberRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다")))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("유저가 존재하지 않습니다");
+        verify(memberService).delete(any(Member.class));
     }
 }
