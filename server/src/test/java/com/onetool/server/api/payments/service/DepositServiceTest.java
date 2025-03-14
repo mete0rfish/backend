68b8fa0424e14b5065e669773044d428d0b70a37
PlusUltraCode <kkd06144@naver.com>
2025-03-14 13:26:31 +0900

    feat(Test):  WithMock 추가 반환값 email

--- server/src/test/java/com/onetool/server/api/payments/service/DepositServiceTest.java
+++ server/src/test/java/com/onetool/server/api/payments/service/DepositServiceTest.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 //@SpringBootTest
 //@ActiveProfiles({"test", "local"})
