57361ae2ca2d1ac87c0dc676275d6f60104ff098
mete0rfish <sungwon326@naver.com>
2025-04-11 15:28:00 +0900

    test(blueprint): Service, Business 테스트 코드 추가

---
+++ server/src/test/java/com/onetool/server/api/blueprint/service/BlueprintInspectionServiceTest.java
@@ -0,0 +1,30 @@
+package com.onetool.server.api.blueprint.service;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class BlueprintInspectionServiceTest {
+
+
+
+    @Test
+    void findAllNotPassedBlueprintsWithPage() {
+    }
+
+    @Test
+    void findBluePrintById() {
+    }
+
+    @Test
+    void updateBlueprintInspectionStatus() {
+    }
+
+    @Test
+    void deleteBlueprintById() {
+    }
+}
