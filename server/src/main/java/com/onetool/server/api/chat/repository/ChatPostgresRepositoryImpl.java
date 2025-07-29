7d0110418291c46839b9adeee73ae7ec01c198fa
mete0rfish <sungwon326@naver.com>
2025-07-29 13:42:21 +0900

    chore: Session을 ConccurentMap.set 콜렉션 사용
    Co-authored-by: PlusUltraCode <kkd06144@naver.com>

--- server/src/main/java/com/onetool/server/api/chat/repository/ChatPostgresRepositoryImpl.java
+++ server/src/main/java/com/onetool/server/api/chat/repository/ChatPostgresRepositoryImpl.java
@@ -14,6 +14,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
+@Primary
 @Repository("chatPostgresRepositoryImpl")
 @RequiredArgsConstructor
 public class ChatPostgresRepositoryImpl implements ChatRepository {
