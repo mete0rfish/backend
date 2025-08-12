3e1fc118bce023238141cbed9a94ba525a26e927
mete0rfish <sungwon326@naver.com>
2025-08-12 19:01:12 +0900

    feat(chat): 상속 의존성 추가
    Co-authored-by: PlusUltraCode <kkd06144@naver.com>

--- server/src/main/java/com/onetool/server/api/chat/handler/TestChatWebSocketHandler.java
+++ server/src/main/java/com/onetool/server/api/chat/handler/TestChatWebSocketHandler.java
@@ -2,6 +2,7 @@ package com.onetool.server.api.chat.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onetool.server.api.chat.domain.ChatMessageQueue;
+import com.onetool.server.api.chat.service.ChatProcessService;
 import com.onetool.server.api.chat.service.ChatService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -10,8 +11,8 @@ import org.springframework.web.socket.WebSocketSession;
 @Component
 public class TestChatWebSocketHandler extends ChatWebSocketHandler {
 
-    public TestChatWebSocketHandler(ObjectMapper objectMapper, ChatService chatService, ChatMessageQueue chatMessageQueue) {
-        super(objectMapper, chatService, chatMessageQueue);
+    public TestChatWebSocketHandler(ObjectMapper objectMapper, ChatService chatService, ChatProcessService chatProcessService, ChatMessageQueue chatMessageQueue) {
+        super(objectMapper, chatService, chatProcessService, chatMessageQueue);
     }
 
     public void handleTextMessageForTest(WebSocketSession session, TextMessage message) throws Exception {
