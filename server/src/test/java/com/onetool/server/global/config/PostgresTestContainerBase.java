aaddd1d5db895296168be339208dd8216ac28e54
mete0rfish <sungwon326@naver.com>
2025-10-15 16:17:33 +0900

    test: postgres testContainers 추가

---
+++ server/src/test/java/com/onetool/server/global/config/PostgresTestContainerBase.java
@@ -0,0 +1,23 @@
+package com.onetool.server.global.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@TestConfiguration
+public class PostgresTestContainerBase {
+
+    @Container
+    public static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+}
